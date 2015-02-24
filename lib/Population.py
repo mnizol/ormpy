8cbaf67059ec571b99c92e22f0d475c09af08936
Matthew Nizol <matthew@nizol.net>
2015-02-24 07:09:34 -0500

    Add Population.Relation and write tests for Population

--- lib/Population.py
+++ lib/Population.py
@@ -10,24 +10,30 @@ import sys
 from lib.ORMMinus import ORMMinus
 
 class Population(object):
-    """ A population of an ORM model. """
+    """ A population of an ORM model. If the model is satisfiable, 
+        then self.object_types and self.fact_types will contain the
+        populations of objects and facts, respectively, that satisfy the
+        constraints in the model.  For an unsatisfiable model, 
+        self.object_types and self.fact_types will both be None."""
 
     DEFAULT_SIZE = 10 #: Default size of object and fact type populations.
 
-    def __init__(self, model):
+    def __init__(self, model, ubound=DEFAULT_SIZE):
+        super(Population, self).__init__()
         self._model = model
-        self.object_types = {}  #: A dictionary of object type populations
-        self.fact_types = {}    #: A dictionary of n-ary relations
-        self.populate()
+
+        #: A dictionary of object type populations.  Specifically, the key is
+        #: the object type name and the value is a list of objects.
+        self.object_types = {}  
+
+        #: A dictionary of fact type populations.  Specifically, the key is the
+        #: fact type name and the value is a :class:`lib.Population.Relation`.
+        self.fact_types = {} 
+   
+        self.populate(ubound=ubound)
 
     def populate(self, ubound=DEFAULT_SIZE):
-        """ Generate a population for the model.  If the model is satisfiable, 
-            then self.object_types and self.fact_types will be 
-            dictionaries whose key is the object_type (fact_type) name and
-            whose value is a :class:`lib.Relation.Relation` that satisfies the
-            constraints in the model.  If the model is unsatisfiable, then 
-            self.object_types and self.fact_types will both be set to None. """
-
+        """ Generate a population for the model. """
         solution = ORMMinus(self._model, ubound=ubound).check()
 
         if solution == None:
@@ -43,8 +49,8 @@ class Population(object):
         for obj_type in self._model.object_types:
             name = obj_type.fullname
             domain = obj_type.domain
-            size = solution[name] 
-            self.object_types[name] = domain.draw(size)
+            size = solution[name]             
+            self.object_types[name] = list(domain.draw(size))
 
     def _populate_fact_types(self, solution):
         pass
@@ -52,11 +58,20 @@ class Population(object):
     def write_csv(self, directory):
         pass
 
-class Relation(object):
-    """ A relation, which is a list of tuples. """
+class Relation(list):
+    """ A relation, which is a list of lists of identical arity. """
     
-    def __init__(self):
-        pass
+    def __init__(self, arity=1):
+        super(Relation, self).__init__()
+        self.arity = arity
+
+    def add(self, tupl):
+        """ Add tuple to the list. """
+        if len(tupl) == self.arity:
+            self.append(tupl)
+        else:
+            raise Exception("Cannot add tuple of arity " + str(len(tupl)) +
+                            " to a Relation of arity " + str(self.arity))
 
 
 
--- test/TestPopulation.py
+++ test/TestPopulation.py
@@ -11,7 +11,7 @@ import os
 from unittest import TestCase
 
 import lib.TestDataLocator as TestDataLocator
-from lib.Population import Population
+from lib.Population import Population, Relation
 from lib.NormaLoader import NormaLoader
 
 class TestPopulation(TestCase):
@@ -28,14 +28,41 @@ class TestPopulation(TestCase):
         self.assertIsNone(pop.object_types)
         self.assertIsNone(pop.fact_types)  
 
-    # TODO: This is not a good test...
     def test_populate_object_types(self):
         """ Test population of object types. """
         fname = os.path.join(self.data_dir, "fact_type_tests.orm")
         model = NormaLoader(fname).model
-        pop = Population(model)
+        pop = Population(model, ubound=5)
 
-        print pop.object_types["ObjectTypes.B"]
+        pop1 = pop.object_types["ObjectTypes.A"]
+        self.assertItemsEqual(pop1, ["A0", "A1", "A2", "A3", "A4"])
+
+        pop2 = pop.object_types["ObjectTypes.A_id"]
+        self.assertItemsEqual(pop2, [0, 1, 2, 3, 4])
+
+        pop3 = pop.object_types["ObjectTypes.B"]
+        self.assertItemsEqual(pop3, ["A", "Dog", "3.567", "12/23/2014"])
+
+    def test_add_to_relation(self):
+        """ Test adding tuple to relation. """
+        rel = Relation(arity=2)
+        rel.add([4, 5])
+        rel.add(["a", "b"])
+
+        self.assertEquals(len(rel), 2)
+        self.assertItemsEqual(rel[0], [4,5])
+        self.assertItemsEqual(rel[1], ["a","b"])
+
+    def test_add_wrong_arity_to_relation(self):
+        """ Test adding a tuple of the wrong arity to a relation. """
+        rel = Relation(arity=2)
         
+        with self.assertRaises(Exception) as ex:
+            rel.add([1])
+        self.assertEquals(ex.exception.message, 
+            "Cannot add tuple of arity 1 to a Relation of arity 2")
+
+
+  
               
 

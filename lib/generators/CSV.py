a741ee1098e0d0d5ce21c96410692268f8612195
Matthew Nizol <matthew@nizol.net>
2015-06-15 18:33:30 -0400

    Add CSV generator file

---
+++ lib/generators/CSV.py
@@ -0,0 +1,18 @@
+##############################################################################
+# Package: ormpy
+# File:    CSV.py
+# Author:  Matthew Nizol
+##############################################################################
+
+""" This module provides a class to generate a directory of CSV files from an 
+    OrmPy Population object.
+"""
+
+import os
+
+class CSV(object):
+    """ Generate a directory of CSV files from an OrmPy Population. """
+
+    def __init__(self, population=None, dirname=None, *args, **kwargs):
+        super(CSV, self).__init__(*args, **kwargs)
+        population.write_csv(dirname)
